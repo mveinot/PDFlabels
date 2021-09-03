cfe08b18979ac4a618e37458cc9f491731dac9a8
Mark (Vic) Veinot <mveinot@gmail.com>
2021-09-02 22:47:24 -0300

    Improvements
    Use updated em() function for special characters
    support printing just first names
    Read subjects from form submission

--- labels.php
+++ labels.php
@@ -1,5 +1,10 @@
 <?php
 
+// print subjects as well?
+$do_subjects = false;
+$do_names = false;
+$do_both = true;
+
 // subclass FPDF to add a header
 require('PDF.inc.php');
 
@@ -7,7 +12,7 @@ require('PDF.inc.php');
 require('label-styles.inc.php');
 
 // import subjects
-require('subjects.inc.php');
+#require('subjects.inc.php');
 
 // import miscelaneous string handling utils
 require('misc.inc.php');
@@ -15,10 +20,16 @@ require('misc.inc.php');
 // retrieve the form data
 $style = isset($_REQUEST['style'])?$_REQUEST['style']:'8161';
 $names_list = isset($_REQUEST['names_list'])?$_REQUEST['names_list']:'';
+$subjects_list = isset($_REQUEST['subjects_list'])?$_REQUEST['subjects_list']:'';
+$first_only = isset($_REQUEST['first_only'])?$_REQUEST['first_only']:'off';
 
 // massage the list of names
 $names = explode("\n", $names_list);
 sort($names);
+
+// massage the list of subjects
+$subjects = explode("\n", $subjects_list);
+sort($subjects);
 
 // create a new PDF object from our subclass
 $pdf = new PDF('P','in','Letter');  // P = Portrait, in = units=inches, Letter = letter sized paper (8.5x11)
@@ -37,9 +48,10 @@ $pdf->SetFont('ComicSansMS','',$styles[$style]['font_size']);
 $pdf->setStyle($styles[$style]);
 
 // print out just the names first
+if ($do_names) {
 foreach ($names as $name)
 {
-	$full_name = normalize_name($name);
+	$full_name = normalize_name($name, $first_only);
 
 	// print the name to the page if it's not empty
 	if ($full_name != '')
@@ -47,15 +59,33 @@ foreach ($names as $name)
 		$pdf->addCell(array($full_name));
 	}
 }
+}
 
 // then print out all the subjects with each name
+if ($do_both) {
 foreach ($subjects as $subject)
 {
 	foreach ($names as $name)
 	{
-		$full_name = normalize_name($name);
-		$pdf->addCell(array(urldecode($subject), $full_name));
+		$full_name = normalize_name($name, $first_only);
+		$pdf->addCell(array(urldecode(em($subject)), $full_name));
 	}
+}
+}
+
+if ($do_subjects) {
+foreach ($subjects as $subject)
+{
+	$pdf->addCell(array(urldecode($subject)));
+}
+foreach ($subjects as $subject)
+{
+	$pdf->addCell(array(urldecode($subject)));
+}
+foreach ($subjects as $subject)
+{
+	$pdf->addCell(array(urldecode($subject)));
+}
 }
 
 // print the PDF to the output
