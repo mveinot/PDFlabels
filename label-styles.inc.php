14efe2b7152e3d86a64f3b2ed9cfd3452d06ff73
Mark (Vic) Veinot <mveinot@gmail.com>
2021-09-02 22:45:35 -0300

    Add template for Avery 48867

--- label-styles.inc.php
+++ label-styles.inc.php
@@ -8,9 +8,9 @@ $avery8160 = array(
         'width' => 2.625,
         'height' => 1,
         'margins' => 0.1875,
-        'top' => 1,
+        'top' => 0.5,
         'h_space' => 0.125,
-        'font_size' => 16
+        'font_size' => 12
         );
 
 $avery8161 = array(
@@ -19,12 +19,24 @@ $avery8161 = array(
         'width' => 4,
         'height' => 1,
         'margins' => 0.164,
-        'top' => 1,
+        'top' => 0.35,
         'h_space' => 0.1719,
         'font_size' => 18
         );
 
+$avery48867 = array(
+        'columns' => 4,
+        'rows' => 20,
+        'width' => 1.75,
+        'height' => 0.5,
+        'margins' => 0.5,
+        'top' => 0.5,
+        'h_space' => 0.270,
+        'font_size' => 14
+        );
+
 $styles['8160'] = $avery8160;
 $styles['8161'] = $avery8161;
+$styles['48867'] = $avery48867;
 
 ?>
